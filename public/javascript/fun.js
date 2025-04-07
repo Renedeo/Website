a88cdc7866d9bbccbcc7a6c1a9d78308d8c87d90
Renedeo <62702704+Renedeo@users.noreply.github.com>
2025-04-07 12:36:07 +0200

    Files structure improvement
    Creation of public repository for all asset needed by index.html

---
+++ public/javascript/fun.js
@@ -0,0 +1,70 @@
+const ratio = 0.4;
+let observer = null;
+
+// Selectionner les element
+const spies = document.querySelectorAll(".data-spy");
+/**
+ *  Fait appel a la fonction activate sur l'element intercepté
+ * 
+ * @param {IntersectionObserverEntry[]} entries
+ * @param {intersectionObserver} observer
+ */
+const callback = function (entries, observer) {
+  entries.forEach(function (entry) {
+    if (entry.isIntersecting) {
+        activate(entry.target);
+    }
+  });
+};
+
+/**
+ *  Ajoute une nouvelle classe à chaque element  
+ * en supprimant les anciens 
+ * 
+ * @param {HTMLElement} element
+ */
+const activate = function (element) {
+  const id = element.getAttribute("id");
+  const anchor = document.querySelector(`a[href="#${id}"]`);
+
+  if (anchor === null) {
+    return null;
+  }
+  anchor.parentElement.parentElement
+    .querySelectorAll(".active ")
+    .forEach((node) => node.classList.remove("active"));
+
+  anchor.classList.add("active");
+};
+
+/**
+ *  Update l'observer de chaque element si la taille
+ *  fenetre a été modifiée
+ * 
+ * @param {NodeListof.<Element>} elems
+ */
+const scrollspy = function (elems) {
+  if (observer !== null) {
+    elems.forEach((elem) => {
+      observer.unobserve(elem);
+    });
+  }
+  const y = Math.round(window.innerHeight * ratio);
+
+  observer = new IntersectionObserver(callback, {
+    rootMargin: `-${window.innerHeight - y - 1}px 0px -${y}px 0px`
+
+  })
+  spies.forEach((spy) => observer.observe(spy));
+}
+
+if (spies.length > 0) {
+  scrollspy(spies);
+  window.addEventListener("resize", () => {
+    scrollspy(spies);
+  });
+}
+
+
+/* ***************************Contact form requirement***************************** */
+
