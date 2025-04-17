387e43b309a0b141fd0a7b691b7fb526a47ad983
Renedeo <62702704+Renedeo@users.noreply.github.com>
2025-04-17 11:35:21 +0200

    Bug : php server does't receive data  with post method

--- public/javascript/script.js
+++ public/javascript/script.js
@@ -23,3 +23,27 @@ const observer = new IntersectionObserver(
 
 /** form */
 
+const sendBtn = document.getElementById("send-btn");
+console.log(sendBtn);
+
+
+const xhr = new XMLHttpRequest() 
+$("form").on('submit', (event) => {
+  event.preventDefault()
+  const url = "./server/send-mail.php?test=test";
+  xhr.addEventListener("readystatechange", function () {
+    if (xhr.readyState === XMLHttpRequest.DONE) {
+      if (xhr.status === 200) {
+        console.log(xhr.response)
+      } else {
+        console.error("Erreur " + xhr.status + " : " + xhr.statusText);
+      }
+    }
+  });
+  
+  xhr.open("POST", url);
+  xhr.setRequestHeader("Content-type", "application/x-www-form-urlencoded");
+  
+  let post = "phase=send&mes=ee"
+  xhr.send(post);
+})
