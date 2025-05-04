f3508595c4c88abee26bd3cf4839137cc24dc205
Renedeo <62702704+Renedeo@users.noreply.github.com>
2025-05-04 17:32:48 +0200

    Fix image size Fix scroll behavior discover media queries

--- public/javascript/scroll-anim.js
+++ public/javascript/scroll-anim.js
@@ -1,6 +1,22 @@
 $(function () {
   const $scrollBox = $(".imglist");
-  let initValue = 1
+  let initValue = 10;
+  // const mediaq = window.matchMedia("(max-width: 900px)");
+  // function updateValue() {
+  //   if (mediaq.matches) {
+  //     initValue = 10;
+  //   } else {
+  //     console.log(mediaq);
+  //     initValue = 1;
+  //   }
+  // }
+  // updateValue();
+  // window.addEventListener("resize", updateValue);
+
+  // mediaq.addEventListener(
+  //   "change",
+  //   (event) => (initValue = event.matches ? 10 : 1)
+  // );
   let scrollSpeedArray = Array($scrollBox.length).fill(initValue);
   const edgeBuffer = initValue; // buffer zone to prevent jitter
 
